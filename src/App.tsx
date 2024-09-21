37a93979cf127add976f6f2a38ffb9eee519b454
Abhinay Thakur <a8h1nay@gmail.com>
2024-09-21 22:58:19 +0530

    build: integrate prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,11 +18,7 @@ function App() {
       </div>
       <h1 className="text-3xl font-bold">Vite + React</h1>
       <div className="card">
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>Count is {count}</Button>
       </div>
       <p className="read-the-docs text-slate-500">
         Click on the Vite and React logos to learn more
