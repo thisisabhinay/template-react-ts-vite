e3ea9ae2d57bbb016ccaabf44c396aef55f2638e
Abhinay Thakur <a8h1nay@gmail.com>
2024-09-21 23:09:11 +0530

    build: setup storybook.js and integrate with tailwind css

---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import { withThemeByClassName } from "@storybook/addon-themes"
+import type { Preview } from "@storybook/react"
+import "../src/index.css"
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "",
+        dark: "dark"
+      },
+      defaultTheme: "light"
+    })
+  ]
+}
+
+export default preview
